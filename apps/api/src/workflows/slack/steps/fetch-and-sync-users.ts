5f9744dd7b350594ffd960c9b9286f790b79adcc
koch <contact@och.dev>
2025-07-22 23:34:27 +0200

    feat: fetch and sync slack data

---
+++ apps/api/src/workflows/slack/steps/fetch-and-sync-users.ts
@@ -0,0 +1,76 @@
+import type { WebClient } from "@slack/web-api";
+import type { Member as SlackMemberResponse } from "@slack/web-api/dist/types/response/UsersListResponse";
+import { generateId } from "better-auth";
+import { eq } from "drizzle-orm";
+import * as schema from "../../../db";
+import type { Db } from "../../../db/db";
+import { isTuple } from "../../../lib/is-tuple";
+
+type FetchAndSyncUsersParams = {
+  slackClient: WebClient;
+  db: Db;
+  integrationId: string;
+};
+
+export async function fetchAndSyncUsers({
+  slackClient,
+  db,
+  integrationId,
+}: FetchAndSyncUsersParams) {
+  for await (const page of slackClient.paginate("users.list", { limit: 999 })) {
+    if (!page.ok) {
+      throw new Error(page.error);
+    }
+
+    const usersToInsert: schema.SlackUserInsert[] = [];
+
+    for (const _user of (page as any).members ?? []) {
+      const user = _user as SlackMemberResponse;
+      if (!user.id) {
+        console.log("Skipping user with no id");
+        continue;
+      }
+      if (!user.name) {
+        console.log("Skipping user with no name");
+        continue;
+      }
+      if (user.deleted) {
+        console.log("Skipping deleted user");
+        continue;
+      }
+
+      usersToInsert.push({
+        id: generateId(),
+        integrationId,
+        slackUserId: user.id,
+        username: user.name,
+        displayName: user.profile?.display_name,
+        email: user.profile?.email,
+        avatarUrl: user.profile?.image_original,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      });
+    }
+
+    const batchUpserts = usersToInsert.map((user) => {
+      return db
+        .insert(schema.slackUser)
+        .values(user)
+        .onConflictDoUpdate({
+          target: schema.slackUser.slackUserId,
+          setWhere: eq(schema.slackUser.slackUserId, user.slackUserId),
+          set: {
+            username: user.username,
+            displayName: user.displayName,
+            email: user.email,
+            avatarUrl: user.avatarUrl,
+            createdAt: new Date(),
+            updatedAt: new Date(),
+          },
+        });
+    });
+    if (isTuple(batchUpserts)) {
+      await db.batch(batchUpserts);
+    }
+  }
+}
